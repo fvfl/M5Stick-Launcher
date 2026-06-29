dd4ee96bd07295b8139f431ec72e021febf6495f
Pirata <bmorcelli@gmail.com>
2026-06-29 16:27:42 -0300

    OTA Update downloaded display label

--- src/display.cpp
+++ src/display.cpp
@@ -1232,18 +1232,22 @@ RESTART:
     }
     options.push_back({"[Refine Search]", [&]() { refine = true; }, ALCOLOR});
 
-    if (!doc["items"][0]["file"].as<String>().isEmpty()) {
-        options.push_back({"[Update all]", [=]() {
-            int count = (int)doc["items"].size();
-            for (int i = 0; i < count; i++) {
-                String fid  = doc["items"][i]["fid"].as<String>();
-                String file = doc["items"][i]["file"].as<String>();
-                String name = doc["items"][i]["name"].as<String>();
-                String ver  = doc["items"][i]["version"].as<String>();
-                downloadFirmware(fid, file, name + "." + ver.substring(0, 10), dwn_path, ver, true);
-                if (returnToMenu) break;
-            }
-        }, ALCOLOR});
+    if (!doc["items"][0]["file"].as<String>().isEmpty() && items != 100) {
+        options.push_back(
+            {"[Update all]",
+             [=]() {
+                 int count = (int)doc["items"].size();
+                 for (int i = 0; i < count; i++) {
+                     String fid = doc["items"][i]["fid"].as<String>();
+                     String file = doc["items"][i]["file"].as<String>();
+                     String name = doc["items"][i]["name"].as<String>();
+                     String ver = doc["items"][i]["version"].as<String>();
+                     downloadFirmware(fid, file, name + "." + ver.substring(0, 10), dwn_path, ver, true);
+                     if (returnToMenu) break;
+                 }
+             },
+             ALCOLOR}
+        );
     }
 
     if (current_page > 1) {
